1d25fe044f21c2cab191623f6bcc9ce0b18bfb2f
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-25 15:25:25 +0300

    Update GunBoomPlugin.cs

--- GunBoomPlugin.cs
+++ GunBoomPlugin.cs
@@ -1,8 +1,5 @@
 using Rocket.Core.Plugins;
 using HarmonyLib;
-using Steamworks;
-using System.Collections.Generic;
-using SDG.Unturned;
 
 namespace GunBoom
 {
@@ -10,9 +7,6 @@ namespace GunBoom
     {
         public static GunBoomPlugin Instance;
         private Harmony harmony;
-        
-        public HashSet<CSteamID> JammedPlayers = new HashSet<CSteamID>();
-        public HashSet<CSteamID> RunawayPlayers = new HashSet<CSteamID>();
 
         protected override void Load()
         {
@@ -20,32 +14,17 @@ namespace GunBoom
             harmony = new Harmony("com.ironandmud.gunboom");
             harmony.PatchAll();
             
-            // ИСПРАВЛЕНО: Используем Provider.onEnemyDisconnected вместо Player
-            Provider.onEnemyDisconnected += OnPlayerDisconnected;
-            
-            Rocket.Core.Logging.Logger.Log("GunBoom: Weapon malfunctions initialized.");
+            Rocket.Core.Logging.Logger.Log("GunBoom: Explosions only. Welcome to the trenches.");
         }
 
         protected override void Unload()
         {
-            Provider.onEnemyDisconnected -= OnPlayerDisconnected;
-            harmony.UnpatchAll();
-            JammedPlayers.Clear();
-            RunawayPlayers.Clear();
-        }
-
-        // Аргумент изменен на SteamPlayer для соответствия событию
-        private void OnPlayerDisconnected(SteamPlayer player)
-        {
-            if (player == null) return;
-            CSteamID sID = player.playerID.steamID;
-            JammedPlayers.Remove(sID);
-            RunawayPlayers.Remove(sID);
+            harmony.UnpatchAll("com.ironandmud.gunboom");
         }
 
         public WeaponConfig GetConfig(ushort id)
         {
-            return Configuration.Instance.Weapons.Find(x => x.WeaponID == id);
+            return Configuration.Instance.Weapons?.Find(x => x.WeaponID == id);
         }
     }
 }
