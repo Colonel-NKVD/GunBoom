e558ff9bf21e226a08f8e55a94133a91150e8387
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-25 11:03:03 +0300

    Update GunBoomPlugin.cs

--- GunBoomPlugin.cs
+++ GunBoomPlugin.cs
@@ -2,7 +2,6 @@ using Rocket.Core.Plugins;
 using HarmonyLib;
 using Steamworks;
 using System.Collections.Generic;
-using SDG.Unturned; // Убедись, что это здесь
 
 namespace GunBoom
 {
@@ -18,23 +17,13 @@ namespace GunBoom
         {
             Instance = this;
             harmony = new Harmony("com.gunboom.malfunctions");
-            harmony.PatchAll();
-            
-            // Используем полное имя типа, чтобы компилятор точно его нашел
-            PlayerEquipment.OnEquipRequested_Global += OnEquip;
+            harmony.PatchAll(); // Эта строчка сама найдет и применит все патчи
         }
 
         protected override void Unload()
         {
-            PlayerEquipment.OnEquipRequested_Global -= OnEquip;
             harmony.UnpatchAll();
             Instance = null;
-        }
-
-        // Здесь явно прописываем SDG.Unturned.EquipmentRequestHandler
-        private void OnEquip(SDG.Unturned.EquipmentRequestHandler handler)
-        {
-            RunawayPlayers.Remove(handler.equipment.player.channel.owner.playerID.steamID);
         }
 
         public WeaponConfig GetConfig(ushort id) => Configuration.Instance.Weapons.Find(x => x.WeaponID == id);
