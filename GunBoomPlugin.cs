15ac88a68434602b4a8bc1e4a365f21a55cd7b61
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-25 10:53:28 +0300

    Update GunBoomPlugin.cs

--- GunBoomPlugin.cs
+++ GunBoomPlugin.cs
@@ -1,50 +1,42 @@
 using Rocket.Core.Plugins;
 using HarmonyLib;
-using SDG.Unturned;
 using Steamworks;
 using System.Collections.Generic;
-using UnityEngine;
+using SDG.Unturned;
 
-public class GunBoomPlugin : RocketPlugin<GunBoomConfiguration>
+namespace GunBoom
 {
-    public static GunBoomPlugin Instance;
-    public const string HarmonyInstanceId = "com.gunboom.plugin";
-    private Harmony harmony;
-
-    // Храним SteamID игроков, у которых заклинило оружие
-    public HashSet<CSteamID> JammedPlayers = new HashSet<CSteamID>();
-    
-    // Храним SteamID игроков, у которых запал спуск
-    public HashSet<CSteamID> RunawayPlayers = new HashSet<CSteamID>();
-
-    protected override void Load()
+    public class GunBoomPlugin : RocketPlugin<GunBoomConfiguration>
     {
-        Instance = this;
-        harmony = new Harmony(HarmonyInstanceId);
-        harmony.PatchAll();
-        
-        // Подписываемся на смену предмета, чтобы сбрасывать состояния, если игрок убрал оружие
-        PlayerEquipment.OnEquipRequested_Global += OnEquipRequested;
-    }
+        public static GunBoomPlugin Instance;
+        private Harmony harmony;
 
-    protected override void Unload()
-    {
-        harmony.UnpatchAll(HarmonyInstanceId);
-        PlayerEquipment.OnEquipRequested_Global -= OnEquipRequested;
-        JammedPlayers.Clear();
-        RunawayPlayers.Clear();
-        Instance = null;
-    }
+        // Состояния игроков
+        public HashSet<CSteamID> JammedPlayers = new HashSet<CSteamID>();
+        public HashSet<CSteamID> RunawayPlayers = new HashSet<CSteamID>();
 
-    private void OnEquipRequested(EquipmentRequestHandler handler)
-    {
-        // Если игрок меняет оружие, сбрасываем баг с западанием спуска
-        var steamId = handler.equipment.player.channel.owner.playerID.steamID;
-        RunawayPlayers.Remove(steamId);
-    }
-    
-    public WeaponConfig GetConfigForWeapon(ushort id)
-    {
-        return Configuration.Instance.Weapons.Find(w => w.WeaponID == id);
+        protected override void Load()
+        {
+            Instance = this;
+            harmony = new Harmony("com.gunboom.malfunctions");
+            harmony.PatchAll();
+            
+            PlayerEquipment.OnEquipRequested_Global += OnEquip;
+        }
+
+        protected override void Unload()
+        {
+            PlayerEquipment.OnEquipRequested_Global -= OnEquip;
+            harmony.UnpatchAll();
+            Instance = null;
+        }
+
+        private void OnEquip(EquipmentRequestHandler handler)
+        {
+            // Сброс западания спуска при смене оружия
+            RunawayPlayers.Remove(handler.equipment.player.channel.owner.playerID.steamID);
+        }
+
+        public WeaponConfig GetConfig(ushort id) => Configuration.Instance.Weapons.Find(x => x.WeaponID == id);
     }
 }
