6d46426db4b0aeee73061229a1ce03f268b9e5eb
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-25 12:03:25 +0300

    Update GunBoomPlugin.cs

--- GunBoomPlugin.cs
+++ GunBoomPlugin.cs
@@ -2,6 +2,7 @@ using Rocket.Core.Plugins;
 using HarmonyLib;
 using Steamworks;
 using System.Collections.Generic;
+using SDG.Unturned;
 
 namespace GunBoom
 {
@@ -9,23 +10,38 @@ namespace GunBoom
     {
         public static GunBoomPlugin Instance;
         private Harmony harmony;
-
+        
+        // Используем HashSet для быстрой проверки состояний
         public HashSet<CSteamID> JammedPlayers = new HashSet<CSteamID>();
         public HashSet<CSteamID> RunawayPlayers = new HashSet<CSteamID>();
 
         protected override void Load()
         {
             Instance = this;
-            harmony = new Harmony("com.gunboom.malfunctions");
-            harmony.PatchAll(); // Эта строчка сама найдет и применит все патчи
+            harmony = new Harmony("com.ironandmud.gunboom");
+            harmony.PatchAll();
+            
+            Player.onPlayerDisconnected += OnPlayerDisconnected;
+            Rocket.Core.Logging.Logger.Log("GunBoom Ready for 1917+ warfare.");
         }
 
         protected override void Unload()
         {
+            Player.onPlayerDisconnected -= OnPlayerDisconnected;
             harmony.UnpatchAll();
-            Instance = null;
+            JammedPlayers.Clear();
+            RunawayPlayers.Clear();
         }
 
-        public WeaponConfig GetConfig(ushort id) => Configuration.Instance.Weapons.Find(x => x.WeaponID == id);
+        private void OnPlayerDisconnected(Player player)
+        {
+            JammedPlayers.Remove(player.channel.owner.playerID.steamID);
+            RunawayPlayers.Remove(player.channel.owner.playerID.steamID);
+        }
+
+        public WeaponConfig GetConfig(ushort id)
+        {
+            return Configuration.Instance.Weapons.Find(x => x.WeaponID == id);
+        }
     }
 }
